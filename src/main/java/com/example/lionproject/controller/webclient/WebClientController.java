776df9d2c4059d0101c9b79e4fb9f3f2956f03bd
HyunSoo730 <whgustn730@gmail.com>
2023-07-24 11:54:05 +0900

    Feat: 서울시 문화행사 공공서비스 예약 Open API JSON 연동

---
+++ src/main/java/com/example/lionproject/controller/webclient/WebClientController.java
@@ -0,0 +1,32 @@
+package com.example.lionproject.controller.webclient;
+
+import com.example.lionproject.domain.WebClientDTO;
+import com.example.lionproject.service.Api.WebClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class WebClientController {
+
+
+    private final WebClientService service;
+
+    /**
+     * 서울시 문화행사 공공서비스예약 정보 Open API JSON 연동 성공.
+     */
+    @GetMapping("/test")
+    public WebClientDTO returnString(@RequestParam Integer startIndex, @RequestParam Integer endIndex) {
+        WebClientDTO res = service.get(startIndex, endIndex);
+        return res;
+    }
+
+    @GetMapping("/test2")
+    public String returnString2(@RequestParam Integer startIndex, @RequestParam Integer endIndex) {
+        String res = service.get2(startIndex, endIndex);
+        return res;
+    }
+
+}
