d37e5b8f62778c6a4a1843f5748f2581e97f9641
Min Seop Kim <k_ms1998@naver.com>
2023-07-30 00:36:58 +0900

    Fix: Application gitignore 해제

---
+++ src/main/java/com/example/lionproject/LionProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.lionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionProjectApplication.class, args);
+    }
+}
