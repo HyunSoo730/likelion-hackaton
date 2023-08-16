59e08b5ddfb0d0229905772efdac31813bf1ad6f
moonseonghui <msh9980@naver.com>
2023-08-16 19:38:30 +0900

    [FE] FEAT : 로그아웃 api 연결

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./navbar.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import userImg from "../../assets/images/user.png";
 import logoImg from "../../assets/images/logo.jpg";
+import axios from "axios";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -14,8 +15,30 @@ export default function Navbar() {
 
   const accessToken = localStorage.getItem("access_token");
   const userName = localStorage.getItem("userName");
+  const jwtToken = localStorage.getItem("jwtToken");
 
   const isLoggedIn = accessToken && userName;
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/auth/kakao/logout", {
+        params: {
+          accessToken: accessToken,
+        },
+      });
+
+      if (response.status === 200) {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("userName");
+        localStorage.removeItem("jwtToken");
+        navigate("/");
+      } else {
+        console.error("로그아웃 실패:", response);
+      }
+    } catch (error) {
+      console.error("로그아웃 오류:", error);
+    }
+  };
 
   return (
     <div className="navbar">
@@ -64,7 +87,6 @@ export default function Navbar() {
             <div className="signIn" onClick={() => handleNavigation("/signin")}>
               로그인
             </div>
-            ｜<div className="signUp">회원가입</div>
           </div>
         )}
         {isLoggedIn && (
@@ -73,7 +95,10 @@ export default function Navbar() {
             onClick={() => handleNavigation("/alarmservice")}
           >
             <img src={userImg} width="30px" alt="" />
-            <div className="signUp">{userName}님</div>
+            <div className="signUp">{userName}님</div> |
+            <div className="signUp" onClick={handleLogout}>
+              로그아웃
+            </div>
           </div>
         )}
       </div>
